7d41cf7f0b765771dafe2f7887101ac754c638be
fshahriar298 <mdfahimshahriar17@gmail.com>
2026-09-12 16:15:22 +0600

    Changes Database SQLIT3 to PostgreSQL

--- EduHub_Learning_Management_API/settings.py
+++ EduHub_Learning_Management_API/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'EduHub_Learning_Management_API.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'eduhub_db',
+        'USER': 'postgres',
+        'PASSWORD': '123',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
@@ -104,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+ 
 # Internationalization
 # https://docs.djangoproject.com/en/6.1/topics/i18n/
 
